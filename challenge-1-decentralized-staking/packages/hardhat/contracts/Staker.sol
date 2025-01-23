b391af38445633a5448311b03605fd2d67883b2d
Milos Dzepina <milos.dzepina@gmail.com>
2025-01-23 13:02:49 +0100

    fix git issues
    Signed-off-by: Milos Dzepina <milos.dzepina@gmail.com>

---
+++ challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
+
+import "hardhat/console.sol";
+import "./ExampleExternalContract.sol";
+
+contract Staker {
+    ExampleExternalContract public exampleExternalContract;
+
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    }
+
+    mapping (address => uint256) public balances;
+    uint256 public constant threshold = 1 ether;
+
+    event Stake(address staker, uint256 amount);
+
+    // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+    // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+    function stake() public payable {
+        address sender = msg.sender;
+        uint256 amount = msg.value;
+        balances[sender] += amount;
+        emit Stake(sender, amount);
+    }
+
+    // After some `deadline` allow anyone to call an `execute()` function
+    // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+
+    // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+
+    // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+
+    // Add the `receive()` special function that receives eth and calls stake()
+}
